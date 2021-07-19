fc3acd74c268f53f825ab17ed4fbbb7d958b7501
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-07-19 15:24:45 -0700

    added prediction for csv on flask app

--- Backend/main.py
+++ Backend/main.py
@@ -1,41 +1,38 @@
-import pickle
-import concurrent.futures
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 import numpy as np
-
+import utils
+import json
 from flask import Flask, request, jsonify
-
+import pandas as pd
 
 pickle_path = "tv_layer.pkl"
 model_path = 'lstm_finetune_novec.h5'
 
-def load_vectorizer(path):
-  from_disk = pickle.load(open(path, "rb"))
-  loaded_vectorizer = TextVectorization.from_config(from_disk['config'])
-  loaded_vectorizer.set_weights(from_disk['weights'])
-  return loaded_vectorizer
+model, vectorizer = utils.load_model_vectorizer(model_path, pickle_path)
 
-
-def load_model_vectorizer(model_path, pickle_path):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-      f1 = executor.submit(tf.keras.models.load_model, model_path)
-      f2 = executor.submit(load_vectorizer, pickle_path)
-      loaded_model = f1.result()
-      loaded_vectorizer = f2.result()
-
-      return loaded_model, loaded_vectorizer
-
-
-model, vectorizer = load_model_vectorizer(model_path, pickle_path)
-
-def predict(sentence):
-  x_vec = vectorizer(np.array([sentence]))
+def predict(review):
+  """
+  Takes in a review and returns a prediction on whether it is a 5 star review
+  """
+  x_vec = vectorizer(np.array([review]))
   prob = model.predict(x_vec)[0]
 
   return tf.round(prob).numpy()[0]
+
+def predict_csv(df):
+  """"
+  Takes in a pandas dataframe of reviews and returns a json string with reviews and predictions
+  """
+  x = df.values
+  x_vec = vectorizer(x)
+  probs = model.predict(x_vec)
+  preds = tf.cast(tf.round(probs), tf.int32)
+  preds = tf.squeeze(preds).numpy().tolist()
+  pred_data = pd.DataFrame.from_dict({'review': np.squeeze(x, axis=-1), 'predictions': preds}, )
+
+  return pred_data.to_json(orient='records')
 
 
 app = Flask(__name__)
@@ -48,6 +45,18 @@ def index():
   prediction = predict(x)
   response = {'prediction':int(prediction)}
   return jsonify(response), 201
+
+@app.route("/predictions", methods=['GET',"POST"])
+def predictions():
+  try:
+    file = request.files['file']
+  except:
+    return jsonify({'error': 'could not read file'})
+
+  df = pd.read_csv(file)
+  response = predict_csv(df)
+
+  return response, 201
 
 
 
---
+++ Backend/utils.py
@@ -0,0 +1,25 @@
+import pickle
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import concurrent.futures
+
+
+def load_vectorizer(path):
+  from_disk = pickle.load(open(path, "rb"))
+  loaded_vectorizer = TextVectorization.from_config(from_disk['config'])
+  loaded_vectorizer.set_weights(from_disk['weights'])
+  return loaded_vectorizer
+
+
+def load_model_vectorizer(model_path, pickle_path):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+      f1 = executor.submit(tf.keras.models.load_model, model_path)
+      f2 = executor.submit(load_vectorizer, pickle_path)
+      loaded_model = f1.result()
+      loaded_vectorizer = f2.result()
+
+      return loaded_model, loaded_vectorizer
+
+
