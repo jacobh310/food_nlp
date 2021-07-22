9e73bde62d8faed1a40b24172917ffd5cfed0f0c
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-07-21 20:20:28 -0700

    updated flask server

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import streamlit as st
 import pandas as pd
-import requests
+from google.cloud import storage
 import utils
+import os
+import json
 
-URL_PREDICT  =  "http://127.0.0.1:5000/predict"
+dev = True
+
+if dev:
+    URL_PREDICTIONS = "http://127.0.0.1:5000/predictions"
+    URL_PREDICT = "http://127.0.0.1:5000/predict"
+else:
+    URL_PREDICT  =  "https://food-nlp-g5yr3ihzpq-uw.a.run.app/predict"
+    URL_PREDICTIONS = "https://food-nlp-g5yr3ihzpq-uw.a.run.app/predictions"
+
+
+BUCKET_NAME = 'food-nlp-data'
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "calm-spring-320419-39eded0b835c.json"
 
 st.markdown("<h1 style='text-align: center; color:#295E61 ;'>Restaurant 5 Star Review Classification</h1>",
             unsafe_allow_html=True)
@@ -22,7 +36,7 @@ if options[0] == mode:
             st.warning("Please enter text. Not a number")
         else:
             try:
-                resp = utils.get_prediction(URL_PREDICT, review)
+                resp = utils.get_predict(URL_PREDICT, review)
                 st.json(resp)
             except:
                 st.warning("Connection timed out. Try again")
@@ -33,13 +47,23 @@ if options[0] == mode:
 if options[1] == mode:
 
     st.write("Upload CSV file filled with unlabeled reviews")
-    uploaded_file = st.file_uploader(label="Upload CSV file", type=["csv"])
+    uploaded_file = st.file_uploader(label="Upload CSV", type=["csv"])
 
     if uploaded_file:
 
-        # resp = utils.get_predictions("http://127.0.0.1:5000/predictions", )
-        # st.json(resp)
-
+        # Convert to JSON and send to Flask server
         df = pd.read_csv(uploaded_file)
-        st.dataframe(df.head())
+        json_data = df.to_json(orient='records')
+        resp = utils.get_predictions(URL_PREDICTIONS, json_data)
+
+        pred_df = pd.DataFrame(resp)
+        st.dataframe(pred_df.head())
+
+        blob_name = f"{uploaded_file.name.split('.')[0]}_predictions.csv"
+
+        client = storage.Client()
+        bucket = client.get_bucket(BUCKET_NAME)
+        blob = bucket.blob(blob_name, chunk_size=262144)
+        blob.upload_from_string(pred_df.to_csv(index=False), 'text/csv')
+        st.success('CSV Saved to GCS')
         ## fix type error expected str bytes or os.pathlike object not uploadedfile
--- utils.py
+++ utils.py
@@ -1,13 +1,22 @@
 import requests
 
-def get_prediction(url, review):
+def get_predict(url, review):
+    """
+    Sends single review to flask server and returns with a prediction
+    """
     data = {"input": review}
     headers = {"Content-Type": "application/json"}
     resp = requests.post(url, headers=headers, json=data)
 
     return resp.json()
 
-def get_predictions(url, file):
+def get_predictions(url, json):
 
-    resp = requests.post(url, files={'file' : open(file, 'rb')})
+    """
+    Sends multiple reviews from a in json format to flask server
+    Returns a json with reviews an predictions
+    """
+    headers = {"Content-Type": "application/json"}
+    resp = requests.post(url, headers=headers, json=json)
+
     return resp.json()
