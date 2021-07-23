71ce14e7ed6ded5de545b8080a48e249a955cb8e
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-07-23 15:55:10 -0700

    edit configs

--- Frontend/config.py
+++ Frontend/config.py
@@ -1,19 +1,18 @@
 import os
-import json
 
 
-
-class Config:
+class DevConfig():
+    URL_PREDICT = "https://food-nlp-g5yr3ihzpq-uw.a.run.app/predict"
+    URL_PREDICTIONS = "https://food-nlp-g5yr3ihzpq-uw.a.run.app/predictions"
     BUCKET_NAME = 'food-nlp-data'
     PROJECT_NAME = 'calm-spring-320419'
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "calm-spring-320419-39eded0b835c.json"
 
-class DevConfig(Config):
-    URL_PREDICTIONS = "http://127.0.0.1:5000/predictions"
-    URL_PREDICT = "http://127.0.0.1:5000/predict"
-    GOOGLE_APPLICATION_CREDENTIALS = "calm-spring-320419-39eded0b835c.json"
 
-# class ProdConfig(Config):
-#     URL_PREDICTIONS = os.environ.get("URL_PREDICTIONS")
-#     URL_PREDICT = os.environ.get("URL_PREDICT")
-#     GOOGLE_APPLICATION_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
+class ProdConfig():
+    URL_PREDICT  =  os.environ.get('URL_PREDICT')
+    URL_PREDICTIONS = os.environ.get('URL_PREDICTIONS')
+    PROJECT_NAME = os.environ.get('PROJECT_NAME')
+    BUCKET_NAME = os.environ.get('BUCKET_NAME')
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-credentials.json"
